4cc98d49af6c3858d87435e3f901041537222a20
ActuallyHex <85820320+ActuallyHex@users.noreply.github.com>
2022-12-19 22:50:49 -0600

    Create 9.11 LAB: Fibonacci sequence (recursion).java

---
+++ Chapter-9/9.11 LAB: Fibonacci sequence (recursion).java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   
+   public static int fibonacci(int n) {
+     
+     if(n == -100)
+     {
+        return -1;
+     }
+     else if(n <= 1)
+     {
+        return n;
+     }
+     
+     return fibonacci(n - 1) + fibonacci(n - 2);
+      
+   }
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int startNum;
+      
+      startNum = scnr.nextInt();
+      System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+   }
+}
