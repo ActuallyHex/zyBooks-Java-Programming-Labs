813b29192d6f149081899d7fb514fdd66fb3ae3c
ActuallyHex <85820320+ActuallyHex@users.noreply.github.com>
2022-12-19 22:39:32 -0600

    Create Dog.java

---
+++ Chapter-8/8.14 LAB: Pet information (derived classes)/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Pet {
+   private String breed;
+
+   public void setBreed(String userBreed) {
+      breed = userBreed;
+   }
+
+   public String getBreed() {
+      return breed;
+   }
+}
