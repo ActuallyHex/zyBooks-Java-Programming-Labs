d6f07a61180c823f085c9e34a66d7c4a637ddc7c
ActuallyHex <85820320+ActuallyHex@users.noreply.github.com>
2022-12-19 22:39:46 -0600

    Create Pet.java

---
+++ Chapter-8/8.14 LAB: Pet information (derived classes)/Pet.java
@@ -0,0 +1,28 @@
+public class Pet {
+
+   protected String name;
+   protected int age;
+
+   public void setName(String userName) {
+      name = userName;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setAge(int userAge) {
+      age = userAge;
+   }
+
+   public int getAge() {
+      return age;
+   }
+
+   public void printInfo() {
+      System.out.println("Pet Information: ");
+      System.out.println("   Name: " + name);
+      System.out.println("   Age: " + age);
+   }
+
+}
